581d73d8ab424b949f7d5dd2e314a50cc00a77d8
neo <g602049338@icloud.com>
2019-11-25 05:09:17 +0800

    更新脚本

--- plugins/wechat/primary.py
+++ plugins/wechat/primary.py
@@ -13,34 +13,40 @@ class WechatApi:
         self.app_id = app_id
         self.app_secret = app_secret
 
-    def get_open_id(self, code: str) -> str:
+    def get_open_id(self, code: str, port: str) -> str:
         """获取open_id"""
-        # if request.get_json(force=True).get('code'):
-        #     return request.get_json(force=True).get('code')
-        # real_code = request.get_json(force=True).get('code')
-        url = f'https://api.weixin.qq.com/cgi-bin/user/info?access_token={self.get_access_token()}&openid=OPENID&lang=zh_CN'
+        account = self.get_account(port=port)
+        url = f'https://api.weixin.qq.com/sns/jscode2session?appid={account["app_id"]}&secret={account["secret"]}&js_code={code}&grant_type=authorization_code'
         result = requests.get(url)
         result = result.json()
+        print(result)
         return result['openid']
 
-    def get_access_token(self) -> str:
-        """优先从redis缓存中获取"""
-        access_token = self.Redis.get(self.access_token_redis_key)
-        if access_token:
-            return access_token
-        else:
-            return self.update_access_token()
+    def get_account(self, port: str) -> dict:
+        """获取小程序账号信息,AppID/AppSecret"""
+        return config.WECHAT_ACCOUNTS[port]
 
-    def update_access_token(self) -> str:
-        """向腾讯服务器请求"""
-        url = f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={config.APP_ID}&secret={config.APP_SECRET}'
 
-        result = requests.get(url=url)
-        result = result.json()
-        access_token = result['access_token']
-        self.Redis.set(self.access_token_redis_key, access_token, ex=7140)
+def get_access_token(self) -> str:
+    """优先从redis缓存中获取"""
+    access_token = self.Redis.get(self.access_token_redis_key)
+    if access_token:
         return access_token
+    else:
+        return self.update_access_token()
 
-    def __call__(self, *args, **kwargs):
-        from init import Redis
-        self.Redis = Redis
+
+def update_access_token(self) -> str:
+    """向腾讯服务器请求"""
+    url = f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={config.APP_ID}&secret={config.APP_SECRET}'
+
+    result = requests.get(url=url)
+    result = result.json()
+    access_token = result['access_token']
+    self.Redis.set(self.access_token_redis_key, access_token, ex=7140)
+    return access_token
+
+
+def __call__(self, *args, **kwargs):
+    from init import Redis
+    self.Redis = Redis
