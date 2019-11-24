0527d97ea351a0a36c46b979fbddafea4ac59030
neo <g602049338@icloud.com>
2019-11-25 03:26:46 +0800

    更新脚本

--- views/wechat/__init__.py
+++ views/wechat/__init__.py
@@ -1,11 +1,6 @@
 """微信服务号相关接口"""
-import config
 from flask import Blueprint
-from plugins.wechat.message_encrypt import WXBizMsgCrypt
 
 api = Blueprint('wechat', __name__, url_prefix='/wechat')
 
 from views.wechat.primary import *
-
-wechat_message_crypt = WXBizMsgCrypt.WXBizMsgCrypt(sToken=config.APP_SERVER_TOKEN,
-                                                   sEncodingAESKey=config.APP_EncodingAESKey, sAppId=config.APP_ID)
--- views/wechat/primary.py
+++ views/wechat/primary.py
@@ -1,9 +1,13 @@
+import config
 from flask import request
-from views.wechat import api, wechat_message_crypt
+from views.wechat import api
+from plugins.wechat.message_encrypt import WXBizMsgCrypt
 
 
 @api.route('/signature/', methods=['GET'])
 def signature():
     """验证消息的确来自微信服务器"""
+    wechat_message_crypt = WXBizMsgCrypt.WXBizMsgCrypt(sToken=config.APP_SERVER_TOKEN,
+                                                       sEncodingAESKey=config.APP_EncodingAESKey, sAppId=config.APP_ID)
     echostr = request.args.get('echostr')
     return echostr
