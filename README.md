a8b0f87676c8073cbd0e2e5a76232dc5962a76a3
neo <g602049338@icloud.com>
2019-11-27 19:39:51 +0800

    管理员通知

--- README.md
+++ README.md
@@ -6,6 +6,11 @@
 
 
     celery -A plugins.private.asynchronous.run_celery:celery worker
+    
 # 全项目更新
 - 启动命令
     
+    
+    
+    python3 /home/ubuntu/neo/HYcore/plugins/private/script/updata.py                # 更新所有项目
+    nohup python3 main.py > core.log &                                              #核心中台挂起命令
--- views/common/primary.py
+++ views/common/primary.py
@@ -66,9 +66,9 @@ def notice_manager():
     admins = Admin.query.filter_by(sms_status=1).all()
 
     phone_numbers = [admin.phone for admin in admins]
-    print(form.params.data)
     if phone_numbers:
-        sms.multi_send(template_id=form.template_id.data, params=form.params.data, sms_sign=config.SMS_SIGN)
+        sms.multi_send(template_id=form.template_id.data, phone_numbers=phone_numbers, params=form.params.data,
+                       sms_sign=config.SMS_SIGN)
 
     return ordinary.result_format()
 
