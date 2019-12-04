f36f2a0727adca17beee7b5405abb587e3c0b425
neo <g602049338@icloud.com>
2019-12-04 20:35:48 +0800

    自定义菜单

--- plugins/wechat/primary.py
+++ plugins/wechat/primary.py
@@ -14,34 +14,60 @@ class WechatApi:
         self.app_secret = app_secret
 
     def get_open_id(self, code: str, port: str) -> str:
-        """获取open_id"""
+        """获取open_id
+        :param code: 微信码
+        :param port: 应用端口号
+        :return:
+        """
         account = self.get_account(port=port)
         url = f'https://api.weixin.qq.com/sns/jscode2session?appid={account["app_id"]}&secret={account["secret"]}&js_code={code}&grant_type=authorization_code'
         result = requests.get(url)
         result = result.json()
-        print(result)
         return result['openid']
 
     def get_account(self, port: str) -> dict:
-        """获取小程序账号信息,AppID/AppSecret"""
+        """获取小程序账号信息,AppID/AppSecret
+        :param port: 应用端口号
+        :return:
+        """
         return config.WECHAT_ACCOUNTS[port]
 
-    def get_access_token(self) -> str:
-        """优先从redis缓存中获取"""
+    def get_access_token(self, port: str) -> str:
+        """优先从redis缓存中获取
+        :param port: 应用端口号
+        :return:
+        """
         from init import Redis
-        access_token = Redis.get(self.access_token_redis_key)
+        access_token = Redis.get(self.access_token_redis_key + port)
         if access_token:
             return access_token
         else:
-            return self.update_access_token()
+            return self.update_access_token(port=port)
 
-    def update_access_token(self) -> str:
-        """向腾讯服务器请求"""
+    def update_access_token(self, port: str) -> str:
+        """向腾讯服务器请求
+        :param port: 应用端口号
+        :return:
+        """
         from init import Redis
-        url = f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={config.APP_ID}&secret={config.APP_SECRET}'
+        account = self.get_account(port=port)
+        url = f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={account["app_id"]}&secret={account["secret"]}'
 
         result = requests.get(url=url)
         result = result.json()
         access_token = result['access_token']
-        Redis.set(self.access_token_redis_key, access_token, ex=7140)
+        Redis.set(self.access_token_redis_key + port, access_token, ex=7140)
         return access_token
+
+    def create_menu(self, body: dict, port: str):
+        """创建微信菜单
+        :param body: 菜单数据
+        :param port: 应用端口号
+        :return:
+        """
+        access_token = self.get_access_token(port=port)
+        print(access_token)
+        url = f'https://api.weixin.qq.com/cgi-bin/menu/create?access_token={access_token}'
+        result = requests.post(url=url, json=body)
+        print(result)
+        print(result.json())
--- views/wechat/primary.py
+++ views/wechat/primary.py
@@ -1,5 +1,7 @@
+import json
 import config
 from flask import request
+from init import wechat_api
 from views.wechat import api
 from plugins.wechat.message_encrypt import WXBizMsgCrypt
 
@@ -15,3 +17,11 @@ def signature():
                                                        sEncodingAESKey=config.APP_EncodingAESKey, sAppId=config.APP_ID)
     return wechat_message_crypt.verity_token_get(signature=signature, timestamp=timestamp, nonce=nonce,
                                                  echo_str=echo_str)
+
+
+@api.route('/menu/create/')
+def create_menu():
+    """创建菜单"""
+    with open("wechat_menu_config.json", "r", encoding="utf-8") as file:
+        menu_config = json.load(file)
+        wechat_api.create_menu(body=menu_config, port="8090")
