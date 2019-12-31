79ac1e772b3b946102a7c38d1da9dd86d31fdd72
neo <g602049338@icloud.com>
2020-01-01 00:21:30 +0800

    脚本更新

--- plugins/private/script/database_update.py
+++ plugins/private/script/database_update.py
@@ -1,6 +1,6 @@
 import os
 
-os.system('source /home/ubuntu/neo/vevns/core/bin/activate;'
+os.system('. /home/ubuntu/neo/vevns/core/bin/activate;'
           'cd /home/ubuntu/neo/HYcore/plugins/private/alembic;'
           'alembic revision --autogenerate -m "备注";'
           'alembic upgrade head;'
