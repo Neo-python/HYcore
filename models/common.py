87b49603922867d4bb9062e04acad99041506cfe
Neo-python <g602049338@icloud.com>
2020-01-19 06:56:15 +0800

    init

--- models/common.py
+++ models/common.py
@@ -3,3 +3,11 @@ from .HYModels import common
 
 class Images(common.ImagesBase):
     """图片"""
+
+
+# class FinanceReceiver(common.FinanceReceiverBase):
+#     """收货人"""
+#
+#
+# class FinanceSender(common.FinanceSenderBase):
+#     """发货人"""
