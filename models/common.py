6c7668794835e90c5b54e8deef2f95ee2dfcabeb
Neo-python <g602049338@icloud.com>
2020-01-19 23:16:02 +0800

    init

--- models/common.py
+++ models/common.py
@@ -5,6 +5,9 @@ class Images(common.ImagesBase):
     """图片"""
 
 
+class SystemParameter(common.SystemParameterBase):
+    """系统参数"""
+
 # class FinanceReceiver(common.FinanceReceiverBase):
 #     """收货人"""
 #
